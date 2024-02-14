4c188fa2427fc914ca3b0e2597b8341561c75a1a
MarS-27 <marchuk1992@gmail.com>
2024-02-14 19:21:13 +0200

    fixed header responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -16,7 +16,7 @@ export const Header: FC = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <header className="pt-2 pb-6 px-4 w-full sticky top-0 z-50 bg-pageBg">
+    <header className="pt-5 sm:pt-2 pb-6 px-4 w-full sticky top-0 z-50 bg-pageBg">
       <div
         className={clsx(
           "flex items-center w-full max-w-container mx-auto",
@@ -26,22 +26,25 @@ export const Header: FC = async () => {
         <Link
           href={ROUTE.RECIPES_START}
           className={clsx(
-            "flex gap-5",
-            !session?.user.token ? "pointer-events-none" : "cursor-pointer"
+            pacifico.className,
+            "flex gap-5 relative",
+            !session?.user.token
+              ? "pointer-events-none"
+              : "cursor-pointer max-sm:after:content-['Personal_Recipes_Book'] max-sm:after:absolute max-sm:after:-top-6 max-sm:after:left-0 max-sm:after:text-s14 max-sm:after:whitespace-nowrap"
           )}
         >
           <Image src="/images/logo.svg" alt="logo" width={40} height={40} />
           <h1
             className={clsx(
               pacifico.className,
-              "text-md24 sm:text-l32 md:text-xL40 font-medium tracking-wider"
+              "text-md24 sm:text-l32 md:text-xL40 font-medium tracking-wider hidden sm:block"
             )}
           >
             Personal Recipes Book
           </h1>
         </Link>
         {session?.user.token ? (
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-3 ">
             <UserAccountButton />
             <AddRecipeButton />
             <LogoutButton />
