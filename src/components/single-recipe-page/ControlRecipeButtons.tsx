c1bf8962da9e526dcf149bbefaabd559af506665
MarS-27 <marchuk1992@gmail.com>
2024-03-18 15:01:15 +0200

    added confirm on delete recipe

--- src/components/single-recipe-page/ControlRecipeButtons.tsx
+++ src/components/single-recipe-page/ControlRecipeButtons.tsx
@@ -6,13 +6,14 @@ import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import { Button } from "../ui/Button";
 import { IconButton } from "../ui/IconButton";
 import { Loader } from "../ui/Loader";
 
 export const ControlRecipeButtons: FC = () => {
   const { ref, isOpenPopup, toggleOpenPopup } = useClosePopupOnClickOutside();
+  const [isOpenDeletingAccept, setOpenDeletingAccept] = useState(false);
   const params = useParams<{ id: string }>();
   const { isDeleting, deleteRecipe } = useDeleteRecipe(params.id);
 
@@ -21,7 +22,10 @@ export const ControlRecipeButtons: FC = () => {
       <IconButton
         iconSrc={isOpenPopup ? "/images/close.svg" : "/images/gear-icon.svg"}
         classNameModificator="w-6 h-6 min-w-6"
-        onClick={() => toggleOpenPopup(!isOpenPopup)}
+        onClick={() => {
+          toggleOpenPopup(!isOpenPopup);
+          setOpenDeletingAccept(false);
+        }}
       />
       <div
         className={clsx(
@@ -44,26 +48,47 @@ export const ControlRecipeButtons: FC = () => {
               <p className="w-full">Edit</p>
             </Button>
           </Link>
-          <Button
-            variant="outlined"
-            onClick={() => deleteRecipe()}
-            disabled={isDeleting}
-          >
-            {isDeleting ? (
-              <Loader classNameModificator="border-t-mainBlue" />
-            ) : (
-              <>
-                <Image
-                  width={24}
-                  height={24}
-                  src="/images/delete-icon.svg"
-                  alt="Delete recipe"
-                  className="min-w-6 mr-1"
-                />
-                <p className="w-full">Delete</p>
-              </>
-            )}
-          </Button>
+          {!isOpenDeletingAccept ? (
+            <Button
+              variant="outlined"
+              onClick={() => setOpenDeletingAccept(true)}
+              disabled={isDeleting}
+            >
+              {isDeleting ? (
+                <Loader classNameModificator="border-t-mainBlue" />
+              ) : (
+                <>
+                  <Image
+                    width={24}
+                    height={24}
+                    src="/images/delete-icon.svg"
+                    alt="Delete recipe"
+                    className="min-w-6"
+                  />
+                  <p className="w-full">Delete</p>
+                </>
+              )}
+            </Button>
+          ) : (
+            <div
+              className={
+                "w-full p-1 flex items-center justify-around gap-1.5 text-sm16 text-center bg-pageBg rounded-md border border-grayStroke-80"
+              }
+            >
+              <p
+                className="w-full p-0.5 underline text-mainRed hover:bg-lightBlue cursor-pointer rounded-md"
+                onClick={() => deleteRecipe()}
+              >
+                Yes
+              </p>
+              <p
+                className="w-full p-0.5 underline text-mainGreen hover:bg-lightBlue cursor-pointer rounded-md"
+                onClick={() => setOpenDeletingAccept(false)}
+              >
+                No
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
