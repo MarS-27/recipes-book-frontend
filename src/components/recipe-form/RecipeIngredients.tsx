37ed1e38b544fc382917e5b620afedac5f6b155b
MarS-27 <marchuk1992@gmail.com>
2024-05-14 15:52:20 +0300

    replace add ingredient and add stage buttons in add recipe form

--- src/components/recipe-form/RecipeIngredients.tsx
+++ src/components/recipe-form/RecipeIngredients.tsx
@@ -28,40 +28,45 @@ export const RecipeIngredients: FC = () => {
   };
 
   return (
-    <div className="flex w-full flex-col gap-4">
-      <div className="flex items-center gap-3 border-b-2 border-b-mainBlue pb-2">
+    <div className="flex w-full flex-col justify-between gap-4">
+      <p className="border-b-2 border-b-mainBlue pb-2 font-semibold">
+        Ingredients:
+      </p>
+      <div className="flex grow flex-col gap-2">
+        {ingredients.map((_, i) => (
+          <div className="flex items-center gap-2" key={i}>
+            <FormInput
+              type="text"
+              placeholder="Ingredient"
+              onEnterKeyDown={handleAddIngrInput}
+              register={register(`ingredients.${i}`, {
+                required: 'Add ingredient!',
+              })}
+              error={errors.ingredients?.[i] as FieldError}
+            />
+            <IconButton
+              iconSrc="/images/delete-icon.svg"
+              classNameModificator="w-7 h-7 min-w-7"
+              onClick={() =>
+                setValue(
+                  'ingredients',
+                  ingredients.filter((_, index) => i !== index),
+                )
+              }
+              disabled={ingredients.length === 1}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center justify-end gap-2">
+        <p className="text-xs12 font-light">Add ingredient</p>
+
         <IconButton
           iconSrc="/images/add-icon.svg"
           classNameModificator="w-9 h-9 min-w-9"
           onClick={handleAddIngrInput}
         />
-        <p className="font-semibold">Ingredients:</p>
       </div>
-
-      {ingredients.map((_, i) => (
-        <div className="flex items-center gap-2" key={i}>
-          <FormInput
-            type="text"
-            placeholder="Ingredient"
-            onEnterKeyDown={handleAddIngrInput}
-            register={register(`ingredients.${i}`, {
-              required: 'Add ingredient!',
-            })}
-            error={errors.ingredients?.[i] as FieldError}
-          />
-          <IconButton
-            iconSrc="/images/delete-icon.svg"
-            classNameModificator="w-7 h-7 min-w-7"
-            onClick={() =>
-              setValue(
-                'ingredients',
-                ingredients.filter((_, index) => i !== index),
-              )
-            }
-            disabled={ingredients.length === 1}
-          />
-        </div>
-      ))}
     </div>
   );
 };
--- src/components/recipe-form/RecipeStages.tsx
+++ src/components/recipe-form/RecipeStages.tsx
@@ -32,8 +32,42 @@ export const RecipeStages: FC = () => {
   };
 
   return (
-    <div className="flex w-full flex-col gap-2">
-      <div className="flex items-center gap-3">
+    <div className="flex w-full flex-col justify-between gap-2">
+      <p className="font-semibold">Stages:</p>
+      <div className="flex grow flex-col gap-2">
+        {fields.map((field, i) => (
+          <div
+            className="flex flex-col gap-3 border-t-2 border-t-mainBlue pt-2"
+            key={field.id}
+          >
+            <div className="flex items-center justify-end gap-2">
+              <p className="text-s14">Stage {field.stageNumber}</p>
+              <IconButton
+                iconSrc="/images/delete-icon.svg"
+                classNameModificator="w-5 h-5 min-w-5"
+                onClick={() => deleteStage(i)}
+                disabled={fields.length === 1}
+              />
+            </div>
+
+            <TextArea
+              placeholder="Stage"
+              register={register(`stages.${i}.description`, {
+                required: 'Add stage description!',
+              })}
+              error={errors.stages?.[i]?.description as FieldError}
+              rows={3}
+            />
+            <AddRecipeImages
+              fieldName={`stages.${i}.imgPath`}
+              updatedImgPath={stages[i].imgPath}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="flex items-center justify-end gap-2">
+        <p className="text-xs12 font-light">Add stage</p>
         <IconButton
           iconSrc="/images/add-icon.svg"
           classNameModificator="w-9 h-9 min-w-9"
@@ -45,37 +79,7 @@ export const RecipeStages: FC = () => {
             })
           }
         />
-        <p className="font-semibold">Stages:</p>
       </div>
-      {fields.map((field, i) => (
-        <div
-          className="flex flex-col gap-3 border-t-2 border-t-mainBlue pt-2"
-          key={field.id}
-        >
-          <div className="flex items-center justify-end gap-2">
-            <p className="text-s14">Stage {field.stageNumber}</p>
-            <IconButton
-              iconSrc="/images/delete-icon.svg"
-              classNameModificator="w-5 h-5 min-w-5"
-              onClick={() => deleteStage(i)}
-              disabled={fields.length === 1}
-            />
-          </div>
-
-          <TextArea
-            placeholder="Stage"
-            register={register(`stages.${i}.description`, {
-              required: 'Add stage description!',
-            })}
-            error={errors.stages?.[i]?.description as FieldError}
-            rows={3}
-          />
-          <AddRecipeImages
-            fieldName={`stages.${i}.imgPath`}
-            updatedImgPath={stages[i].imgPath}
-          />
-        </div>
-      ))}
     </div>
   );
 };
